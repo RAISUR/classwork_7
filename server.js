72a71e790b56a13e17c504d1adee62608ab0c8ef
raisur <raisur.rahman234@gmail.com>
2018-11-14 02:05:36 +0000

    alif

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 var http = require('http');
-var fs=require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    fs.readFile('index.html',function(err,data){
-      if(err){
-      return console.log("File read error")
-      }
-    res.end(data);
-    })
-  });
-  server.listen(process.env.PORT||3000, process.env.IP, function(){
+var express = require('express');
+
+var app = express();
+
+var server = http.Server(app);
+
+app.get('/', function(request,response){
+  response.sendFile(__dirname+'/index.html');
+});
+
+app.get('/about', function(request,response){
+  response.sendFile(__dirname+'/about.html');
+});
+
+// var fs = require('fs');
+//   var server = http.createServer(function(req, res){
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     fs.readFile('index.html', function(err, data){
+//         if(err){
+//             return console.log("File read error");
+//         }
+//         res.end(data);
+//     });
+//     // res.end("Hello World!");
+//   });
+  server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
-  
-  
-   var http=require('http')
